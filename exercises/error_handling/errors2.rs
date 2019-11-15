a8a3762cbf4d288da0be98145eace9b0993bae49
Cedric Chee <cedric@invictusbyte.com>
2019-11-15 06:14:46 +0000

    Final solution for errors2 - shortcut ? operator

--- exercises/error_handling/errors2.rs
+++ exercises/error_handling/errors2.rs
@@ -16,7 +16,7 @@
 // There are at least two ways to implement this that are both correct-- but
 // one is a lot shorter! Execute `rustlings hint errors2` for hints to both ways.
 
-// I AM NOT DONE
+// `I AM NOT DONE`
 
 use std::num::ParseIntError;
 
@@ -25,10 +25,8 @@ pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let cost_per_item = 5;
     let qty = item_quantity.parse::<i32>();
 
-    match item_quantity.parse::<i32>() {
-        Ok(qty) => Ok(qty * cost_per_item + processing_fee),
-        Err(err) => Err(err),
-    }
+    let qty = item_quantity.parse::<i32>()?;
+    Ok(qty * cost_per_item + processing_fee)
 }
 
 #[cfg(test)]
