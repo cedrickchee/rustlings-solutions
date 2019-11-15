360b9611551cf223a7bc8bcadfc44e55332925db
Cedric Chee <cedric@invictusbyte.com>
2019-11-15 05:08:47 +0000

    3rd solution - make fn mutably borrow its arg

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -4,24 +4,17 @@
 
 // I AM NOT DONE
 
-/// borrowing mutably
 fn main() {
     let mut vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(&mut vec0);
+    fill_vec(&mut vec0);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
-
-    vec1.push(88);
-
-    println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: &mut Vec<i32>) -> Vec<i32> {
+fn fill_vec(vec: &mut Vec<i32>) {
     vec.push(22);
     vec.push(44);
     vec.push(66);
-
-    vec.to_vec()
 }
