d4f7d9125604f826c33cd719f787d2822635516b
Cedric Chee <cedric@invictusbyte.com>
2019-11-15 05:13:44 +0000

    4rd solution - make fn borrow its arg

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -4,17 +4,28 @@
 
 // I AM NOT DONE
 
+/// Make `fill_vec` borrow its argument instead of taking ownership of it,
+/// and then copy the data within the function in order to return an owned
+/// `Vec<i32>`
 fn main() {
-    let mut vec0 = Vec::new();
+    let vec0 = Vec::new();
 
-    fill_vec(&mut vec0);
+    let mut vec1 = fill_vec(&vec0);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
+
+    vec1.push(88);
+
+    println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: &mut Vec<i32>) {
+fn fill_vec(vec: &Vec<i32>) -> Vec<i32> {
+    let mut vec = vec.clone(); // copy the data
+
     vec.push(22);
     vec.push(44);
     vec.push(66);
+
+    vec // return an owned Vec<i32>
 }
