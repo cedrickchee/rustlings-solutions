ad5cb3ed41f3b63937a976aad46dc446d9ebb93f
Cedric Chee <cedric@invictusbyte.com>
2019-11-15 03:41:13 +0000

    Solution for tests3

--- exercises/tests/tests3.rs
+++ exercises/tests/tests3.rs
@@ -4,7 +4,7 @@
 // we expect to get when we call `is_even(5)`.
 // Execute `rustlings hint tests3` for hints :)
 
-// I AM NOT DONE
+// `I AM NOT DONE`
 
 pub fn is_even(num: i32) -> bool {
     num % 2 == 0
@@ -16,6 +16,12 @@ mod tests {
 
     #[test]
     fn is_true_when_even() {
-        assert!();
+        assert!(is_even(8));
+    }
+
+    #[test]
+    fn got_result() {
+        assert_eq!(is_even(3), false);
+        assert_eq!(is_even(10), true);
     }
 }
