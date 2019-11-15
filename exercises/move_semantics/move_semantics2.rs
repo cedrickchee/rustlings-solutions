3af1da6733fabcc9dc76c89a4c5a060815d3f1cf
Cedric Chee <cedric@invictusbyte.com>
2019-11-15 05:00:31 +0000

    2nd solution - borrowing mutably for move_semantics2

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -4,11 +4,11 @@
 
 // I AM NOT DONE
 
+/// borrowing mutably
 fn main() {
-    let vec0: Vec<i32> = Vec::new();
-    let vec2 = Vec::new();
+    let mut vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec2);
+    let mut vec1 = fill_vec(&mut vec0);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
@@ -18,12 +18,10 @@ fn main() {
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
-
+fn fill_vec(vec: &mut Vec<i32>) -> Vec<i32> {
     vec.push(22);
     vec.push(44);
     vec.push(66);
 
-    vec
+    vec.to_vec()
 }
